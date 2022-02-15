073e69e3dcf6202ffd70d76f8e6d5ceff0820a4d
alejandropch <alejandropachas1@gmail.com>
2022-02-15 15:54:29 -0500

    creating an apollo server

--- server/src/index.js
+++ server/src/index.js
@@ -1,10 +1,21 @@
 const express = require("express")
+const { ApolloServer, gql } = require("apollo-server-express")
+const resolvers = require("./resolvers")
+const typeDefs = require("./schemas")
 
 const app = express()
 const PORT = process.env.PORT || 3000
+
 app.get("/", (req, res) => res.send("hello!"))
-try {
-  app.listen(PORT, () => console.log("listening on port", PORT))
-} catch (e) {
-  console.log(e)
+
+async function start() {
+  try {
+    const server = new ApolloServer({ typeDefs, resolvers })
+    await server.start()
+    server.applyMiddleware({ app })
+    app.listen(PORT, () => console.log("listening on port", PORT))
+  } catch (e) {
+    console.log(e)
+  }
 }
+start()
