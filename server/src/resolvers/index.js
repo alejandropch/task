120fc778d059456ac578c46fda6c07b35080fa20
alejandropch <alejandropachas1@gmail.com>
2022-02-15 16:06:09 -0500

    setting up a basic sample 'database' and user query was set up

--- server/src/resolvers/index.js
+++ server/src/resolvers/index.js
@@ -1,6 +1,28 @@
+const user = [
+  {
+    first_name: "aaa",
+    last_name: "aaa",
+    email: "aaa",
+    note: "aaa",
+  },
+  {
+    first_name: "bbb",
+    last_name: "bbb",
+    email: "bbb",
+    note: "bbb",
+  },
+  {
+    first_name: "ccc",
+    last_name: "ccc",
+    email: "ccc",
+    note: "ccc",
+  },
+]
 const resolvers = {
   Query: {
-    hello: () => "Helloo",
+    users: (parent, args, context) => {
+      return user
+    },
   },
 }
 module.exports = resolvers
