5dfd7471458e6c466e03145dd0b7381dc4836b1b
alejandropch <alejandropachas1@gmail.com>
2022-02-15 17:23:14 -0500

    using the User model for adding a and find all users using query and mutations types respectively

--- server/src/graphql/resolvers/index.js
+++ server/src/graphql/resolvers/index.js
@@ -1,6 +1,17 @@
+const User = require("../../models/User.model")
+
 const resolvers = {
   Query: {
-    users: async () => {},
+    users: async () => {
+      const users = await User.find({})
+      return users
+    },
+  },
+  Mutation: {
+    addUser: async (parent, args) => {
+      const newUser = await User.create(args)
+      return newUser
+    },
   },
 }
 module.exports = resolvers
