841d1a8ce0224b1a80c8bf0ac574285092726f57
alejandropch <alejandropachas1@gmail.com>
2022-02-15 19:23:32 -0500

    implementing addional security

--- server/src/index.js
+++ server/src/index.js
@@ -1,6 +1,11 @@
 require("dotenv").config()
 const express = require("express")
 const { ApolloServer, gql } = require("apollo-server-express")
+const cors = require("cors")
+const helmet = require("helmet")
+const rateLimit = require("express-rate-limit")
+const xss = require("xss-clean")
+
 const resolvers = require("./graphql/resolvers")
 const typeDefs = require("./graphql/schemas")
 const connectDB = require("./db")
@@ -8,6 +13,19 @@ const app = express()
 const PORT = process.env.PORT || 3000
 const URI = process.env.MONGO_URI
 app.get("/", (req, res) => res.send("hello!"))
+app.set("trust proxy", 1)
+
+app.use(
+  rateLimit({
+    windowMs: 15 * 60 * 1000,
+    max: 100,
+    standardHeaders: true,
+    legacyHeaders: false,
+  })
+)
+app.use(cors())
+app.use(helmet())
+app.use(xss())
 
 async function start() {
   try {
