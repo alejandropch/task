804caa4d8adfc498cc8bccc0b2a523e1e3e4a2c2
alejandropch <alejandropachas1@gmail.com>
2022-02-16 06:18:30 -0500

    setting ap the form container file and using the query mutation of graphql

---
+++ client/src/containers/Form.tsx
@@ -0,0 +1,32 @@
+import React, { useRef, useState } from "react"
+import { useAddMutation } from "../graphql/mutations.graphql"
+import FormInputs from "../components/FormInputs"
+export default function Form({ refetch }: any) {
+  const form = useRef<HTMLFormElement>(null)
+  const [message, setMessage] = useState("")
+  const addUser = useAddMutation(setMessage)
+  const handleClick = async () => {
+    const formData = new FormData(form.current!)
+    const User = Object.fromEntries(formData)
+    const { first_name, last_name, email, note } = User
+    const newUser = await addUser({
+      variables: { first_name, last_name, email, note },
+    })
+    if (!newUser.errors) {
+      setMessage("User created!")
+      refetch()
+    }
+  }
+
+  return (
+    <>
+      <form className="form-group" ref={form}>
+        <FormInputs />
+        <button type="button" className="btn btn-primary" onClick={handleClick}>
+          Submit
+        </button>
+      </form>
+      <p className="justify-content-center ">{message}</p>
+    </>
+  )
+}
